8090fb351f8f62fb55cf61577e62943623bbf794
42BeerSoldier <felipediaz@42labs.cl>
2025-08-20 12:58:02 -0400

    feat: add dynamic form and field components with routing and JSON schema

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dynamic-form', component: DynamicFormComponent },
+  { path: '', redirectTo: '/dynamic-form', pathMatch: 'full' },
+  { path: '**', redirectTo: '/dynamic-form' }
+];
---
+++ src/app/dynamic-field/dynamic-field.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-field',
+  imports: [],
+  templateUrl: './dynamic-field.html',
+  styleUrl: './dynamic-field.css'
+})
+export class DynamicField {
+
+}
